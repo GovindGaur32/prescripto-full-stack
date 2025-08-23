bf0d7c5ccadf23008df7ebba90bb918bf87f6766
GovindGaur32 <govindgaur327@gmail.com>
2025-08-23 17:31:26 +0530

    m

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,7 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors({
-  origin: process.env.CLIENT_URL || process.env.ADMIN_URL,
-  credentials: true
-}))
+app.use(cors())
 
 // api endpoints
 app.use("/api/user", userRouter)
